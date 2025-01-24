7c4285e1ce85204c503771810ee66abdb78388f4
nsjames <skyandbeyond@gmail.com>
2025-01-24 13:42:00 -0500

    deployments

--- deployments/jungle.ts
+++ deployments/jungle.ts
@@ -9,7 +9,7 @@
 
 module.exports = async (deployer) => {
 
-    const contract = await deployer.deploy('youraccount', 'build/contract', {
+    const contract = await deployer.deploy('mustbenothin', 'build/system', {
         // adds the `eosio.code` permission to the contract account's active permission
         // so that you can send inline actions from the contract in its name
         addCode: true
@@ -17,6 +17,10 @@ module.exports = async (deployer) => {
         console.error(err)
         process.exit(1);
     })
+
+    const MAX_SUPPLY = 2100000000;
+    const maxSupply = `${MAX_SUPPLY}.0000 XYZ`;
+    await contract.actions.init([maxSupply]).send('mustbenothin@active');
 
     // do other stuff here...
 }
--- fuckyea.config.js
+++ fuckyea.config.js
@@ -1,12 +1,11 @@
 module.exports = {
     networks:{
         jungle: {
-            // node_url: 'https://eos.greymass.com',
+            node_url: 'https://jungle4.greymass.com/',
             chain: 'Jungle4',
             accounts: [
                 {
-                    name: 'youraccount',
-                    // permission: 'owner', // defaults to active
+                    name: 'mustbenothin',
                     private_key: process.env.PRIVATE_KEY
                 }
             ]
